8ff68d2aba7c1d46b785710305feaf92cca22f4a
zzx <23090284@qq.com>
2015-12-07 11:30:12 +0800

    first commit

---
+++ MyFramework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFramework
+//
+//  Created by xian on 15/12/7.
+//  Copyright © 2015年 xian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyFramework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFramework
+//
+//  Created by xian on 15/12/7.
+//  Copyright © 2015年 xian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
